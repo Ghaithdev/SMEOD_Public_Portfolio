30a1b202065e2230406cddf186fba8ebe0001092
Ghaithdev <140157218+Ghaithdev@users.noreply.github.com>
2025-03-19 14:29:07 +0000

    Delete Coding/Minor Scripts/Python/QualMS reader.py

--- Coding/Minor Scripts/Python/QualMS reader.py
+++
@@ -1,9 +0,0 @@
-from tkinter import filedialog
-import os
-
-folder_choice=filedialog.askdirectory(title="Open a folder or something, idk")
-print(f"Chosen folder: {folder_choice}")
-files=os.listdir(folder_choice)
-print(files)
-for file in files:
-    print(file)
