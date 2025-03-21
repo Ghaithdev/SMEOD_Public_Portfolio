cf0555fdefbf9bc91db2323d79f14b40c0da78aa
Ghaithdev <140157218+Ghaithdev@users.noreply.github.com>
2025-03-21 09:46:51 +0000

    Delete Coding/Testing_ideas/Module_writer_tool_with_date_stamper.py

--- Coding/Testing_ideas/Module_writer_tool_with_date_stamper.py
+++
@@ -1,5 +0,0 @@
-import datetime
-now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-print(now)
-fhand=open(input("Please input filename including file extension\n>"),"a")
-fhand.write(input("What would you like to append to the chosen text file?\n>")+"\nTheses additions were made on:"+ str(now)+"\n")
